b7cbd15684ade4aedb3c62159a8cc708a6b23523
fdevos1 <fdevos@DESKTOP-F0GM91V.>
2024-04-29 08:47:06 -0300

    created & consume context

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,22 +1,13 @@
+import WeatherProvider from "@/Context/WeatherContext";
 import Layout from "@/Layouts/Layout";
-import { Link, Head } from "@inertiajs/react";
 
-export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const handleImageError = () => {
-        document
-            .getElementById("screenshot-container")
-            ?.classList.add("!hidden");
-        document.getElementById("docs-card")?.classList.add("!row-span-1");
-        document
-            .getElementById("docs-card-content")
-            ?.classList.add("!flex-row");
-        document.getElementById("background")?.classList.add("!hidden");
-    };
-
+export default function Welcome() {
     return (
         <>
             <Layout>
-                <p>Hello World</p>
+                <WeatherProvider>
+                    <p>Hello World</p>
+                </WeatherProvider>
             </Layout>
         </>
     );
