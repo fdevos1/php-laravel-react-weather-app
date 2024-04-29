2f348cccf86809e81e2141e05c45cde07ad5434c
fdevos1 <fdevos@DESKTOP-F0GM91V.>
2024-04-28 23:28:28 -0300

    first commit

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <>
+            <div className="w-full h-full flex justify-center items-center">
+                <p className="text-3xl">Hello World</p>
+            </div>
+
+        </>
+    );
+}
